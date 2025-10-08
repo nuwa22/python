9915a9e68ec8e0253ed0261832d0ef5383750878
Suresh <snt4800nuwa@gmail.com>
2025-10-08 11:03:58 +0530

    dataType and input output✏️

---
+++ dataType/dataType.py
@@ -0,0 +1,18 @@
+name = "Suresh" # String
+age = 25 # Integer
+height = 5.7 # Float
+is_student = True # Boolean
+list = ["apple", "banana", "cherry"] # List
+tuple = ("apple", "banana", "cherry") # Tuple
+set = {"apple", "banana", "cherry"} # Set
+dict = {"name": "Suresh", "age": 25, "height": 5.7, "is_student": True} # Dictionary
+
+
+print("name:", name)
+print("age:", age)
+print("height:", height)
+print("is_student:", is_student)
+print("list:", list)
+print("tuple:", tuple)
+print("set:", set)
+print("dict:", dict)
---
+++ input/Output/inputAndOutput.py
@@ -0,0 +1,6 @@
+3 # input
+number = int(input("Enter a number: "))
+print("You entered:", number)
+
+name = input("Enter your name: ")
+print("Hello,", name)
